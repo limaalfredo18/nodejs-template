cc2dfba95d0aba8751c75d3b774ef58416f9400d
limaalfredo18 <limaalfredo18@gmail.com>
2022-10-05 16:37:52 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { printHello } from '#Lib/hello.js';
+import connectDB from '#Config/db.js';
+import '#Config/env.js';
+import httpServer from '#Config/https.js';
 
-printHello();
+const start = async () => {
+    const PORT = process.env.PORT;
+    const MONGODB_URL = process.env.MONGODB_URL;
+
+    await connectDB(MONGODB_URL);
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server is starting at port ${PORT} ...`);
+    });
+};
+
+start();
